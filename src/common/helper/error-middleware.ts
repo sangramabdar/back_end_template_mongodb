a9285c44f3b53856d9d891b4c504a6a7726deaf2
sangramabdar <abdarsangram2697@gmail.com>
2022-12-01 13:57:07 +0530

    updated

--- src/common/helper/error-middleware.ts
+++ src/common/helper/error-middleware.ts
@@ -3,23 +3,27 @@ import { CustomError } from "./exceptions";
 import ResponseBodyBuilder from "./response-body-builder";
 
 async function errorHandlingMiddleWare(
-  error: CustomError,
+  error: CustomError | string | any,
   request: Request,
   response: Response,
   next: NextFunction
 ) {
   let responseBody = new ResponseBodyBuilder<string>();
 
-  responseBody.setError(error.message);
-
-  if (!error.statusCode) {
-    //to handle implicit error
-    response.status(500);
-    responseBody.setStatusCode(500);
+  if (typeof error === "string") {
+    responseBody.setError(error);
   } else {
-    //to handle explicit error
-    response.status(error.statusCode);
-    responseBody.setStatusCode(error.statusCode);
+    responseBody.setError(error.message);
+
+    if (!error.statusCode) {
+      //to handle implicit error
+      response.status(500);
+      responseBody.setStatusCode(500);
+    } else {
+      //to handle explicit error
+      response.status(error.statusCode);
+      responseBody.setStatusCode(error.statusCode);
+    }
   }
 
   return response.json(responseBody);
